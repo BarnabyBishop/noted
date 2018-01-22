64fdbc7cb7e0b0183fee00e0fa616e2f9f57f005
Barnaby <barneyb@gmail.com>
2018-01-22 13:49:01 +1100

    Fixed issue where new items would be saved without any text or titles.

--- src/middleware/save-list-item.js
+++ src/middleware/save-list-item.js
@@ -2,13 +2,15 @@ import { saveListItem } from '../service';
 
 export default store => next => async action => {
     if (action.type !== 'SAVE_LIST_ITEM') return next(action);
+
     const state = store.getState();
     const list = state.list;
     const listItem = list.find(item => item.id === action.id);
 
     next(action);
 
-    if (listItem && listItem.dirty) {
+    // Only save an item if it has changed (dirty) and has some title or text filled in
+    if (listItem && listItem.dirty && listItem.title && listItem.text) {
         next({ type: 'SAVING_LIST_ITEM', id: action.id });
         const savedListItem = await saveListItem(listItem);
         return next({ type: 'SAVED_LIST_ITEM', id: action.id, item: savedListItem });
