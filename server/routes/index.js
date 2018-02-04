6ecf9b45942c4eef4002c169ee07ac794e62a632
Barnaby <barneyb@gmail.com>
2018-02-04 21:55:00 +1100

    Moved server folder into the root directory and gave it its own package.json.

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ import saveListItemRoute from './save-list-item-route';
 
 const router = require('koa-router')();
 
+router.get('/', (ctx, next) => { ctx.send(200) });
 router.get('/api/get-list', getListRoute);
 router.post('/api/save-list-item', saveListItemRoute);
 
--- src/start-dev.js
+++ src/start-dev.js
@@ -1,4 +1,4 @@
-import server from './server';
+import server from '../server';
 import client from './client';
 
 server();
