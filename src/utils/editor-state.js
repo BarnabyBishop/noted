7fd6a16d543e34dd0c0b1cb8048ab9c19ff81d39
Barnaby <barneyb@gmail.com>
2018-03-20 11:28:09 +1100

    Updated decorator strategory code. Added URL, attempted code (WIP)

--- src/utils/editor-state.js
+++ src/utils/editor-state.js
@@ -14,26 +14,71 @@ const tagBlock = (Tag, props) => {
     return <Tag>{props.children}</Tag>;
 };
 
+const Url = (props) => (
+    <a href={props.decoratedText} target="_blank">{props.children}</a>
+);
+
+const Code = (props) => (
+    <pre className="code-block"><code>{props.children}</code></pre>
+);
+
+const types = {
+    h1: {
+        regex: /^#\s.*/g,
+        tag: 'h1'
+    },
+    h2: {
+        regex: /^##\s.*/g,
+        tag: 'h2'
+    },
+    h3: {
+        regex: /^###\s.*/g,
+        tag: 'h3'
+    },
+    strong: {
+        regex: /\*.+\*/g,
+        tag: 'strong'
+    },
+    em: {
+        regex: /_.+_/g,
+        tag: 'em'
+    },
+    url: {
+        regex: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+    },
+    code: {
+        regex: /```(.|\n)[^```]*```/g
+    }
+}
+
 const compositeDecorator = new CompositeDecorator([
     {
-        strategy: (...args) => tagStrategy(/^#\s.*/g, ...args),
-        component: (props) => tagBlock('h1', props)
+        strategy: (...args) => tagStrategy(types.h1.regex, ...args),
+        component: (props) => tagBlock(types.h1.tag, props)
     },
     {
-        strategy: (...args) => tagStrategy(/^##\s.*/g, ...args),
-        component: (props) => tagBlock('h2', props)
+        strategy: (...args) => tagStrategy(types.h2.regex, ...args),
+        component: (props) => tagBlock(types.h2.tag, props)
     },
     {
-        strategy: (...args) => tagStrategy(/^###\s.*/g, ...args),
-        component: (props) => tagBlock('h3', props)
+        strategy: (...args) => tagStrategy(types.h3.regex, ...args),
+        component: (props) => tagBlock(types.h3.tag, props)
     },
     {
-        strategy: (...args) => tagStrategy(/\*.+\*/g, ...args),
-        component: (props) => tagBlock('strong', props)
+        strategy: (...args) => tagStrategy(types.strong.regex, ...args),
+        component: (props) => tagBlock(types.strong.tag, props)
     },
     {
-        strategy: (...args) => tagStrategy(/_.+_/g, ...args),
-        component: (props) => tagBlock('em', props)
+        strategy: (...args) => tagStrategy(types.em.regex, ...args),
+        component: (props) => tagBlock(types.em.tag, props)
+    },
+    {
+        strategy: (...args) => tagStrategy(types.url.regex, ...args),
+        component: Url
+    },
+    {
+        strategy: (...args) => tagStrategy(types.code.regex, ...args),
+        component: Code
     }
 ]);
 
