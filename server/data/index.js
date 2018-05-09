fd89b6025441454e32a8c4d689b4c4250d85932f
Barnaby <barneyb@gmail.com>
2018-05-09 17:42:36 +1000

    Initial GraphQL + Apollo setup

--- server/data/index.js
+++ server/data/index.js
@@ -1,6 +1,8 @@
 import Sequelize from 'sequelize';
+import moment from 'moment';
+import { makeExecutableSchema } from 'graphql-tools';
 
-export default (dbHost) => {
+export default dbHost => {
     const sequelize = new Sequelize(dbHost);
     const ListItem = sequelize.define('list_item', {
         id: {
@@ -29,5 +31,50 @@ export default (dbHost) => {
         }
     });
 
-    return { sequelize, ListItem };
+    // The GraphQL schema in string form
+    const typeDefs = `
+        type ListItem {
+            id: String
+            sortOrder: Int
+            created: String
+            completed: String
+            title: String
+            text: String
+            height: Int
+            createdAt: String
+            updatedAt: String
+        }
+        type Query {
+            allItems: [ListItem]
+            itemBySearch(term: String): [ListItem],
+            itemByDate(date: String): [ListItem]
+        }
+        `;
+
+    // The resolvers
+    const resolvers = {
+        Query: {
+            allItems(_, args) {
+                return ListItem.findAll();
+            },
+            itemBySearch(_, args) {
+                return ListItem.findAll({ where: { title: { $ilike: `%${args.term}%` } } });
+            },
+            itemByDate(_, args) {
+                // moment(date).isSame(item.created, 'day') ||
+                //     moment(date).isBetween(item.created, item.completed, 'day', '[]');
+                const startOfDay = moment(args.date).startOf('day');
+                const endOfDay = moment(args.date).endOf('day');
+                return ListItem.findAll({ where: { created: { $gte: startOfDay, $lt: endOfDay } } });
+            }
+        }
+    };
+
+    // Put together a schema
+    const schema = makeExecutableSchema({
+        typeDefs,
+        resolvers
+    });
+
+    return { sequelize, ListItem, schema };
 };
--- server/index.js
+++ server/index.js
@@ -6,8 +6,7 @@ import Koa from 'koa';
 import views from 'koa-views';
 import bodyParser from 'koa-bodyparser';
 import chalk from 'chalk';
-import { assign } from 'lodash';
-import router from './routes';
+import buildRouter from './routes';
 import initData from './data';
 import error from './middleware/error';
 import auth from './middleware/auth';
@@ -16,6 +15,8 @@ const DEFAULT_NODE_PORT = process.env.NODE_PORT || 4321;
 const data = initData(process.env.DB_HOST);
 
 const app = new Koa();
+
+const router = buildRouter(data);
 
 Object.assign(app.context, {
     data,
@@ -34,6 +35,7 @@ app.use(error);
 app.use(auth);
 
 app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(DEFAULT_NODE_PORT, () => {
     console.log(chalk.green(`API server listening on ${DEFAULT_NODE_PORT}.`));
