363f6662ac1028844f34565760712a661b8ee98f
Barnaby <barneyb@gmail.com>
2018-06-19 16:34:47 +1000

    Cleaned up some commented code.

--- server/index.js
+++ server/index.js
@@ -3,14 +3,9 @@
 /*************************/
 require('dotenv').config({ silent: true });
 import express from 'express';
-
-// import Koa from 'koa';
-// import views from 'koa-views';
-// import bodyParser from 'koa-bodyparser';
 import chalk from 'chalk';
 import getRouter from './routes';
 import initData from './data';
-// import error from './middleware/error';
 
 const DEFAULT_NODE_PORT = process.env.NODE_PORT || 4321;
 const data = initData(process.env.DB_HOST);
@@ -20,15 +15,6 @@ const app = express();
 const router = getRouter(data);
 
 app.set('data', data);
-// Object.assign(app.context, {
-//     data
-// });
-
-// body parser
-// app.use(bodyParser());
-
-// basic error handling
-// app.use(error);
 
 app.use(router);
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,44 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import saveListItemRoute from './save-list-item-route';
 
 const router = express.Router();
 
-// import bodyParser from 'koa-bodyparser';
-// import { graphqlKoa, graphiqlKoa } from 'apollo-server-koa';
-import getListRoute from './get-list-route';
-import getTagsRoute from './get-tags-route';
-import saveListItemRoute from './save-list-item-route';
-
-// const router = require('koa-router')();
-
 // Calling as a function so the GraphQL schema is available
 export default data => {
-    // middleware that is specific to this router
-    // router.use(function timeLog(req, res, next) {
-    //     console.log('Time: ', Date.now());
-    //     next();
-    // });
-    // // define the home page route
-    // router.get('/', function(req, res) {
-    //     res.send('Birds home page');
-    // });
-    // // define the about route
-    // router.get('/about', function(req, res) {
-    //     res.send('About birds');
-    // });
-
     router.get('/', (req, res) => {
         res.send(200);
     });
-    // router.get('/api/get-list', getListRoute);
-    // router.get('/api/get-tags', getTagsRoute);
-    router.post('/api/save-list-item', saveListItemRoute);
-    // // koaBody is needed just for POST.
-    // router.post('/api/graphql', bodyParser(), graphqlKoa({ schema: data.schema }));
-    // router.get('/api/graphql', graphqlKoa({ schema: data.schema }));
 
-    // router.get('/api/graphiql', graphiqlKoa({ endpointURL: '/api/graphql' }));
+    router.post('/api/save-list-item', saveListItemRoute);
 
     router.use('/api/graphql', bodyParser.json(), graphqlExpress({ schema: data.schema }));
     router.get('/api/graphiql', graphiqlExpress({ endpointURL: '/api/graphql' })); // if you want GraphiQL enabled
