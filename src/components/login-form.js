f7700714da0f16d134b5d8dcc8c8ef6c662ae771
Barnaby <barneyb@gmail.com>
2018-07-20 15:56:01 +1000

    Introduced styled components and styled login form

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 export default class LoginForm extends React.Component {
     state = {
         email: null,
@@ -17,21 +19,58 @@ export default class LoginForm extends React.Component {
 
     render() {
         return (
-            <div className="form">
-                <input
-                    type="text"
-                    placeholder="Email"
-                    onInput={this.updateField.bind(this, 'email')}
-                    value={this.state.email}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    onInput={this.updateField.bind(this, 'password')}
-                    value={this.state.password}
-                />
-                <button type="submit" onClick={this.login.bind(this)} />
-            </div>
+            <FormContainer>
+                <Form>
+                    <Input
+                        type="text"
+                        placeholder="Email"
+                        onInput={this.updateField.bind(this, 'email')}
+                        value={this.state.email}
+                    />
+                    <Input
+                        type="password"
+                        placeholder="Password"
+                        onInput={this.updateField.bind(this, 'password')}
+                        value={this.state.password}
+                    />
+                    <Submit type="submit" onClick={this.login.bind(this)}>
+                        Login
+                    </Submit>
+                </Form>
+            </FormContainer>
         );
     }
 }
+
+const FormContainer = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 100px;
+`;
+
+const Form = styled.div`
+    display: flex;
+    align-items: flex-end;
+    flex-direction: column;
+`;
+
+const Input = styled.input`
+    display: block;
+    width: 250px;
+    padding: 10px;
+    border-radius: 5px;
+    border: solid 1px #ddd;
+    font-size: 17px;
+    margin-bottom: 10px;
+`;
+
+const Submit = styled.button`
+    padding: 10px;
+    font-size: 13px;
+    font-weight: bold;
+    background-color: cornflowerblue;
+    color: #ffffff;
+    border-radius: 5px;
+    width: 100px;
+`;
