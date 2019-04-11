8ce769d27118463efcf329b3c5031ded7c8292a4
Barnaby <barneyb@gmail.com>
2019-04-11 16:40:18 +1000

    Updated rich text editor from draftjs to slate.

--- src/components/details.js
+++ src/components/details.js
@@ -1,128 +1,10 @@
 import React, { Component } from 'react';
-import { Editor, EditorState, RichUtils, AtomicBlockUtils } from 'draft-js';
 import styled from 'styled-components';
 import moment from 'moment';
-import buildEditorState from '../utils/editor-state';
 import './details.css';
+import RichEditor from './editor';
 
 export default class Details extends Component {
-    constructor(props) {
-        super(props);
-        const text = props.selectedListItem ? props.selectedListItem.text : '';
-
-        this.state = {
-            dragging: false,
-            editorState: buildEditorState(text)
-        };
-        this.focus = () => this.refs.editor.focus();
-        this.autosave = null;
-    }
-
-    componentDidUpdate(prevProps) {
-        const { selectedListItem } = this.props;
-        if (
-            selectedListItem &&
-            (!prevProps.selectedListItem || selectedListItem.id !== prevProps.selectedListItem.id)
-        ) {
-            this.setState({ editorState: buildEditorState(selectedListItem.text) });
-        }
-    }
-
-    onChange(editorState) {
-        this.setState({ editorState });
-        clearTimeout(this.autosave);
-        this.autosave = setTimeout(this.save.bind(this), 1000);
-    }
-
-    handleBlur() {
-        this.save();
-    }
-
-    save() {
-        const plainText = this.state.editorState.getCurrentContent().getPlainText();
-        if (this.props.selectedListItem && this.props.selectedListItem.text !== plainText) {
-            this.props.actions.updateListItemText(this.props.selectedListItem.id, plainText);
-            this.props.actions.saveListItem(this.props.selectedListItem.id);
-        }
-    }
-
-    onTab(e) {
-        const maxDepth = 4;
-        this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth));
-    }
-
-    onDragEnter(e) {
-        e.stopPropagation();
-        e.preventDefault();
-        this.setState({ dragging: true });
-    }
-
-    onDragLeave(e) {
-        e.stopPropagation();
-        e.preventDefault();
-        this.setState({ dragging: false });
-    }
-
-    onDrop(e) {
-        e.stopPropagation();
-        e.preventDefault();
-
-        var dt = e.dataTransfer;
-        var files = dt.files;
-
-        var firstImage;
-        for (var i = 0; i < files.length; i++) {
-            firstImage = files[i];
-            var imageType = /^image\//;
-
-            if (!imageType.test(firstImage.type)) {
-                continue;
-            }
-            break;
-        }
-
-        const reader = new FileReader();
-        reader.onload = this.insertImage.bind(this);
-        // (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
-        reader.readAsDataURL(firstImage);
-    }
-
-    insertImage(e) {
-        const imageSrc = e.target.result;
-        const { editorState } = this.state;
-        const contentState = editorState.getCurrentContent();
-        const contentStateWithEntity = contentState.createEntity('image', 'IMMUTABLE', {
-            src: imageSrc,
-            inline: true
-        });
-        const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
-        const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity });
-        this.props.updateArticleField(
-            this.props.field,
-            AtomicBlockUtils.insertAtomicBlock(newEditorState, entityKey, ' ')
-        );
-        this.setState(
-            {
-                // editorState: AtomicBlockUtils.insertAtomicBlock(newEditorState, entityKey, ' '),
-                showURLInput: false,
-                urlValue: ''
-            },
-            () => {
-                setTimeout(() => this.focus(), 0);
-            }
-        );
-    }
-
-    mediaBlockRenderer(block) {
-        if (block.getType() === 'atomic') {
-            return {
-                component: Image,
-                editable: false
-            };
-        }
-        return null;
-    }
-
     render() {
         if (!this.props.selectedListItem)
             return (
@@ -133,7 +15,7 @@ export default class Details extends Component {
 
         const { id, title, height, completed, created, sortOrder, createdAt, updatedAt } = this.props.selectedListItem;
 
-        const { editorState } = this.state;
+        // const { editorState } = this.state;
 
         return (
             <div className="details">
@@ -153,35 +35,12 @@ export default class Details extends Component {
                     </InfoIcon>
                 </InfoPanel>
                 <div className="RichEditor-root">
-                    <div
-                        className="RichEditor-editor"
-                        onClick={this.focus.bind(this)}
-                        onDragEnter={this.onDragEnter.bind(this)}
-                        onDragLeave={this.onDragLeave.bind(this)}
-                        onDrop={this.onDrop.bind(this)}
-                    >
-                        <Editor
-                            blockRendererFn={this.mediaBlockRenderer.bind(this)}
-                            blockStyleFn={getBlockStyle}
-                            editorState={editorState}
-                            onChange={this.onChange.bind(this)}
-                            onTab={this.onTab.bind(this)}
-                            ref="editor"
-                            spellCheck={true}
-                        />
+                    <div className="RichEditor-editor">
+                        <RichEditor selectedListItem={this.props.selectedListItem} actions={this.props.actions} />
                     </div>
                 </div>
             </div>
         );
-    }
-}
-
-function getBlockStyle(block) {
-    switch (block.getType()) {
-        case 'blockquote':
-            return 'fancy-blockquote';
-        default:
-            return null;
     }
 }
 
---
+++ src/components/editor.js
@@ -0,0 +1,245 @@
+import Plain from 'slate-plain-serializer';
+import { Editor } from 'slate-react';
+
+import Prism from 'prismjs';
+import React from 'react';
+
+/**
+ * Add the markdown syntax to Prism.
+ */
+
+// eslint-disable-next-line
+Prism.languages.markdown=Prism.languages.extend("markup",{}),Prism.languages.insertBefore("markdown","prolog",{blockquote:{pattern:/^>(?:[\t ]*>)*/m,alias:"punctuation"},code:[{pattern:/^(?: {4}|\t).+/m,alias:"keyword"},{pattern:/``.+?``|`[^`\n]+`/,alias:"keyword"}],title:[{pattern:/\w+.*(?:\r?\n|\r)(?:==+|--+)/,alias:"important",inside:{punctuation:/==+$|--+$/}},{pattern:/(^\s*)#+.+/m,lookbehind:!0,alias:"important",inside:{punctuation:/^#+|#+$/}}],hr:{pattern:/(^\s*)([*-])([\t ]*\2){2,}(?=\s*$)/m,lookbehind:!0,alias:"punctuation"},list:{pattern:/(^\s*)(?:[*+-]|\d+\.)(?=[\t ].)/m,lookbehind:!0,alias:"punctuation"},"url-reference":{pattern:/!?\[[^\]]+\]:[\t ]+(?:\S+|<(?:\\.|[^>\\])+>)(?:[\t ]+(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\)))?/,inside:{variable:{pattern:/^(!?\[)[^\]]+/,lookbehind:!0},string:/(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\))$/,punctuation:/^[\[\]!:]|[<>]/},alias:"url"},bold:{pattern:/(^|[^\\])(\*\*|__)(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,lookbehind:!0,inside:{punctuation:/^\*\*|^__|\*\*$|__$/}},italic:{pattern:/(^|[^\\])([*_])(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,lookbehind:!0,inside:{punctuation:/^[*_]|[*_]$/}},url:{pattern:/!?\[[^\]]+\](?:\([^\s)]+(?:[\t ]+"(?:\\.|[^"\\])*")?\)| ?\[[^\]\n]*\])/,inside:{variable:{pattern:/(!?\[)[^\]]+(?=\]$)/,lookbehind:!0},string:{pattern:/"(?:\\.|[^"\\])*"(?=\)$)/}}}}),Prism.languages.markdown.bold.inside.url=Prism.util.clone(Prism.languages.markdown.url),Prism.languages.markdown.italic.inside.url=Prism.util.clone(Prism.languages.markdown.url),Prism.languages.markdown.bold.inside.italic=Prism.util.clone(Prism.languages.markdown.italic),Prism.languages.markdown.italic.inside.bold=Prism.util.clone(Prism.languages.markdown.bold); // prettier-ignore
+
+/**
+ * The markdown preview example.
+ *
+ * @type {Component}
+ */
+
+class RichEditor extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const text = this.props.selectedListItem ? this.props.selectedListItem.text : '';
+
+        this.state = {
+            editorState: Plain.deserialize(text)
+        };
+        console.log(this.state.editorState);
+        this.autosave = null;
+    }
+
+    componentDidUpdate(prevProps) {
+        const { selectedListItem } = this.props;
+        if (
+            selectedListItem &&
+            (!prevProps.selectedListItem || selectedListItem.id !== prevProps.selectedListItem.id)
+        ) {
+            this.setState({ editorState: Plain.deserialize(selectedListItem.text) });
+        }
+    }
+
+    onChange(slateStuff) {
+        console.log(slateStuff);
+        this.setState({ editorState: slateStuff.value });
+        clearTimeout(this.autosave);
+        this.autosave = setTimeout(this.save.bind(this), 1000);
+    }
+
+    handleBlur() {
+        this.save();
+    }
+
+    save() {
+        const plainText = Plain.serialize(this.state.editorState);
+        if (this.props.selectedListItem && this.props.selectedListItem.text !== plainText) {
+            this.props.actions.updateListItemText(this.props.selectedListItem.id, plainText);
+            this.props.actions.saveListItem(this.props.selectedListItem.id);
+        }
+    }
+    /**
+     *
+     * Render the example.
+     *
+     * @return {Component} component
+     */
+
+    render() {
+        return (
+            <Editor
+                placeholder="Nothing to see here..."
+                value={this.state.editorState}
+                renderMark={this.renderMark}
+                decorateNode={this.decorateNode}
+                onChange={this.onChange.bind(this)}
+            />
+        );
+    }
+
+    /**
+     * Render a Slate mark.
+     *
+     * @param {Object} props
+     * @param {Editor} editor
+     * @param {Function} next
+     * @return {Element}
+     */
+
+    renderMark = (props, editor, next) => {
+        const { children, mark, attributes } = props;
+
+        switch (mark.type) {
+            case 'bold':
+                return <strong {...attributes}>{children}</strong>;
+
+            case 'code':
+                return <code {...attributes}>{children}</code>;
+
+            case 'italic':
+                return <em {...attributes}>{children}</em>;
+
+            case 'underlined':
+                return <u {...attributes}>{children}</u>;
+
+            case 'title': {
+                return (
+                    <span
+                        {...attributes}
+                        style={{
+                            fontWeight: 'bold',
+                            fontSize: '20px',
+                            margin: '10px 0 10px 0',
+                            display: 'inline-block'
+                        }}
+                    >
+                        {children}
+                    </span>
+                );
+            }
+
+            case 'punctuation': {
+                return (
+                    <span {...attributes} style={{ opacity: 0.2 }}>
+                        {children}
+                    </span>
+                );
+            }
+
+            case 'list': {
+                return (
+                    <span
+                        {...attributes}
+                        style={{
+                            paddingLeft: '10px',
+                            lineHeight: '10px',
+                            fontSize: '20px'
+                        }}
+                    >
+                        {children}
+                    </span>
+                );
+            }
+
+            case 'hr': {
+                return (
+                    <span
+                        {...attributes}
+                        style={{
+                            borderBottom: '2px solid #000',
+                            display: 'block',
+                            opacity: 0.2
+                        }}
+                    >
+                        {children}
+                    </span>
+                );
+            }
+
+            default: {
+                return next();
+            }
+        }
+    };
+
+    /**
+     * Define a decorator for markdown styles.
+     *
+     * @param {Node} node
+     * @param {Function} next
+     * @return {Array}
+     */
+
+    decorateNode(node, editor, next) {
+        const others = next() || [];
+        if (node.object !== 'block') return others;
+
+        const string = node.text;
+        const texts = node.getTexts().toArray();
+        const grammar = Prism.languages.markdown;
+        const tokens = Prism.tokenize(string, grammar);
+        const decorations = [];
+        let startText = texts.shift();
+        let endText = startText;
+        let startOffset = 0;
+        let endOffset = 0;
+        let start = 0;
+
+        function getLength(token) {
+            if (typeof token === 'string') {
+                return token.length;
+            } else if (typeof token.content === 'string') {
+                return token.content.length;
+            } else {
+                return token.content.reduce((l, t) => l + getLength(t), 0);
+            }
+        }
+
+        for (const token of tokens) {
+            startText = endText;
+            startOffset = endOffset;
+
+            const length = getLength(token);
+            const end = start + length;
+
+            let available = startText.text.length - startOffset;
+            let remaining = length;
+
+            endOffset = startOffset + remaining;
+
+            while (available < remaining) {
+                endText = texts.shift();
+                remaining = length - available;
+                available = endText.text.length;
+                endOffset = remaining;
+            }
+
+            if (typeof token !== 'string') {
+                const dec = {
+                    anchor: {
+                        key: startText.key,
+                        offset: startOffset
+                    },
+                    focus: {
+                        key: endText.key,
+                        offset: endOffset
+                    },
+                    mark: {
+                        type: token.type
+                    }
+                };
+
+                decorations.push(dec);
+            }
+
+            start = end;
+        }
+
+        return [...others, ...decorations];
+    }
+}
+
+/**
+ * Export.
+ */
+export default RichEditor;
