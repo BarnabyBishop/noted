0367de505eaefa4815f14d6f594daf968acb4c42
Barney <barneyb@gmail.com>
2020-04-02 21:57:40 +1100

    Mobile styles for tag list

--- src/components/tag-list.js
+++ src/components/tag-list.js
@@ -26,15 +26,28 @@ const Container = styled.div`
     div {
         cursor: pointer;
     }
+
+    @media (max-width: 420px) {
+        display: flex;
+        justify-content: center;
+    }
 `;
 
 const Tag = styled.span`
     ${p => p.selected && `color: #4990fe;`}
     padding-left: 5px;
+
+    @media (max-width: 420px) {
+        display: none;
+    }
 `;
 
 const Row = styled.div`
     padding: 3px 0px;
+
+    @media (max-width: 420px) {
+        padding: 0px 5px;
+    }
 `;
 
 const Icon = styled.span`
